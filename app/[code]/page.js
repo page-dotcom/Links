15455c56fa8f8e442b1f2df55b382673bc35bcb9
page-dotcom <asepkonha@gmail.com>
2026-02-04 14:05:32 +0700

    Update page.js

--- app/[code]/page.js
+++ app/[code]/page.js
@@ -3,18 +3,10 @@ import { redirect } from 'next/navigation';
 
 export default async function RedirectPage({ params }) {
   const { code } = params;
-
-  const { data } = await supabase
-    .from('links')
-    .select('*')
-    .eq('slug', code)
-    .single();
-
+  const { data } = await supabase.from('links').select('*').eq('slug', code).single();
   if (data) {
-    // Update Hit Count
     await supabase.from('links').update({ clicks: (data.clicks || 0) + 1 }).eq('id', data.id);
     redirect(data.original_url);
   }
-
-  return <div style={{ textAlign: 'center', marginTop: '50px' }}>{"404 - Link Gak Ada!"}</div>;
+  return <div style={{ textAlign: 'center', marginTop: '100px' }}>{"404 - Link Gak Ada!"}</div>;
 }
