ed588671e7fffebb082383f44d6a647f03a2aff4
page-dotcom <asepkonha@gmail.com>
2026-02-05 04:32:13 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import { siteConfig } from '../lib/config';
 export default function Header() {
   return (
     <header>
@@ -9,7 +9,7 @@ export default function Header() {
             <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
             <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
           </svg>
-          ShortPro
+          {siteConfig.name}
         </Link>
       </div>
     </header>
