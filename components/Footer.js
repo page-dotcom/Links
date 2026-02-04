9c645085410e6966f14dbbeef9fc7036558f7653
page-dotcom <asepkonha@gmail.com>
2026-02-04 14:04:13 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ background: '#f4f4f4', padding: '1rem', textAlign: 'center', borderTop: '1px solid #ddd', marginTop: 'auto' }}>
+      <p>{"© 2026 FreeShorten Project"}</p>
+    </footer>
+  );
+}
