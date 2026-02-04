351664c74db127636059b4b27e61de24c2272655
page-dotcom <asepkonha@gmail.com>
2026-02-05 04:52:42 +0700

    Update page.js

--- app/privacy/page.js
+++ app/privacy/page.js
@@ -1,7 +1,7 @@
 "use client";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { siteConfig } from '../lib/config';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import { siteConfig } from '../../lib/config';
 
 export default function PrivacyPolicy() {
   return (
